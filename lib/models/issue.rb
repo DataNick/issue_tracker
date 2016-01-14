417d61b7c04ee4a792fae6d380d2e59ed9a55f18
DataNick <DataNick@users.noreply.github.com>
2016-01-14 16:17:22 -0500

    define fields for issue model that allows model to persist data inside the issue's collection

--- lib/models/issue.rb
+++ lib/models/issue.rb
@@ -1,4 +1,8 @@
 class Issue
   include Mongoid::Document
   include Mongoid::Timestamps::Updated
+
+  #Define set of fields that belong to Issue
+  field :name, type: String
+  field :description, type: String
 end
