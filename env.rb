f14782d4eb4447f0d8676388bd667229c8fcddab
DataNick <DataNick@users.noreply.github.com>
2016-01-14 17:03:09 -0500

    renamed file in file load path

--- env.rb
+++ env.rb
@@ -1,2 +1,6 @@
 require "bundler"
 Bundler.require
+
+#connect yml file to application
+#env variable responsible for load settings, gems and dependencies - mongodb has settings
+Mongoid.load! File.expand_path("../mongoid.yml", __FILE__), :development
