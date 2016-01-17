1423732992414c710c3eab3c7e2785504c992be6
DataNick <DataNick@users.noreply.github.com>
2016-01-16 20:20:45 -0500

    Added list of status symbols in a constant

--- lib/models/issue.rb
+++ lib/models/issue.rb
@@ -1,8 +1,11 @@
 class Issue
+  STATUSES = %i(new open needs_info cannot_duplicate closed) #LIST OF SYMBOLS
+
   include Mongoid::Document
   include Mongoid::Timestamps::Updated
 
   #Define set of fields that belong to Issue
   field :name, type: String
   field :description, type: String
+  field :status, type: Symbol
 end
